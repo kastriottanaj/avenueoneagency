0ea526be5e2d561a755fda162c4427c404ec7722
Kastriot <kastriot@Kastriots-MacBook-Air.local>
2026-02-10 22:00:34 +0100

    Prepare Django app for Render deployment

--- polepositionautomation/settings.py
+++ polepositionautomation/settings.py
@@ -28,11 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mx%l0-=8+&#tu$8j3&xkfnj4ie&3kz5k(9o7&$$cy2r_pm^*#9'
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-if not SECRET_KEY:
-    raise RuntimeError("SECRET_KEY environment variable not set")
-
 
 
 
