796e4eff728de65045c140bef98fbae1d3d7e970
Kastriot <kastriot@Kastriots-MacBook-Air.local>
2026-02-10 21:12:47 +0100

    Move SECRET_KEY to environment variables

--- polepositionautomation/settings.py
+++ polepositionautomation/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cv0c#guqen-(lu#n4fmq)(5)xo(6$ao4#guep+spc=ztnnbvmm'
+SECRET_KEY = 'mx%l0-=8+&#tu$8j3&xkfnj4ie&3kz5k(9o7&$$cy2r_pm^*#9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
