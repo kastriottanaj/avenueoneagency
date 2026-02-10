0be2c684f1792edf748a45c398a54c71eed83dfe
Kastriot <kastriot@Kastriots-MacBook-Air.local>
2026-02-10 22:56:41 +0100

    change the labels

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Ihr Name", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(label="Ihre E-Mail", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    message = forms.CharField(label='Nachricht', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    name = forms.CharField(label="Your Name", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="Your Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
